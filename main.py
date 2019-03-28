c4b45c28415e796dfe80ba43dba903b454e28f31
Priyaj2776 <Priyaj2776@gmail.com>
2019-03-27 22:05:23 -0500

    updated main.py and added caesar.py

--- main.py
+++ main.py
@@ -1,41 +1,47 @@
-from flask import Flask
+from flask import Flask, request
+from caesar import rotate_string
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
-form = """<!DOCTYPE html>
-
+form = """
+<!DOCTYPE html>
 <html>
     <head>
         <style>
-            form {
+            form {{
                 background-color: #eee;
                 padding: 20px;
                 margin: 0 auto;
                 width: 540px;
                 font: 16px sans-serif;
                 border-radius: 10px;
-            }
-            textarea {
+            }}
+            textarea {{
                 margin: 10px 0;
                 width: 540px;
                 height: 120px;
-            }
+            }}
         </style>
     </head>
     <body>
       <form method = "Post">
         <label for="rot">Rotate By:</label>
-        <input type = "text" name = "rot" value = "0" /><br>
-        <textarea rows = "10" name = "text"></textarea><br>
+        <input type = "text" name = "rot" value = "{0}" /><br>
+        <textarea rows = "10" name = "text">{1}</textarea><br>
         <input type = "Submit" value = "Submit Query" />
-
-      
       </form>
     </body>
 </html>
-"""           
+"""    
 @app.route("/")
 def index():
-    return form
+    return form.format(0,'')
 
+@app.route("/", methods=['POST'])
+def encrypt():
+    rotate = int(request.form['rot'])
+    mytext = request.form['text']
+    return form.format(rotate,rotate_string(mytext,rotate)) 
+    
+    
 app.run()
